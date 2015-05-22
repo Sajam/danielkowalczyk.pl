5812ff7a86ddb5e634945b5ee93ba8d2896d497b
Daniel Kowalczyk <d.sajam@gmail.com>
2015-05-22 08:10:25 +0200

    CodeListing improved, multiple persistent lines allowed

--- static/javascript/code-listing.js
+++ static/javascript/code-listing.js
@@ -6,78 +6,55 @@
   var CodeListing = window.CodeListing = function ($listing, options) {
     this.$listing = $listing;
     this.options = options;
-    this.codeLines = undefined;
-    this.codeLinesCount = undefined;
-    this.numberingCharactersCount = undefined;
-
-    this._options = undefined;
-    this._persistentLine = undefined;
+    this.lines = undefined;
+    this.persistentLines = [];
 
     return this.init();
   };
 
   CodeListing.prototype.init = function () {
-    this.prepareContent()
+    return this
+      .prepareContent()
       .bindEvents()
       .buildListing();
-
-    return this;
   };
 
   CodeListing.prototype.prepareContent = function () {
-    this._options = $.extend(
-      true,
-      {
-        className: 'code-listing-default',
-        lineNumbers: true,
-        interactive: true,
-        numberingStart: 1,
-        monospaceCharacterWidth: 10,
-        horizontalPadding: 10
-      },
-      this.options
-    );
-
-    this.$listing
-      .addClass(this._options.className)
-      .data('CodeListing', this);
-
-    this.codeLines = this.$listing.html().split('\n');
-    this.codeLinesCount = this.codeLines.length;
-    this.numberingCharactersCount = this.codeLinesCount.toString().length;
+    this.options = $.extend(true, {}, CodeListing.defaultOptions, this.options);
+    this.$listing.addClass(this.options.className);
+    this.lines = this.$listing
+      .html()
+      .split('\n');
 
     return this;
   };
 
-  // @TODO: optimize events listeners (maybe don't create separate listeners for each instance?).
   CodeListing.prototype.bindEvents = function () {
     var _this = this;
 
-    this.$listing.on('mouseenter', 'span', function () {
-      _this.toggleActiveLine(parseInt($(this).attr('data-line'), 10));
-    });
+    if (this.options.interactive) {
+      this.$listing
+        .on('mouseenter', '[' + CodeListing.lineAttr + ']', function () {
+          _this.toggleActiveLine(
+            CodeListing.getLineNumber(this)
+          );
+        })
+        .on('mouseleave', this.clearActiveLine.bind(this))
+        .on('click', '[' + CodeListing.lineAttr + ']', function () {
+          _this.togglePersistentLine(
+            CodeListing.getLineNumber(this)
+          );
+        });
+    }
 
-    this.$listing.on('click', 'span', function () {
-      _this.togglePersistentLine(parseInt($(this).attr('data-line'), 10), true);
-    });
-
-    this.$listing.on('mouseleave', function () {
-      _this.clearActiveLine();
-    });
-
-    this.$listing.on('click', '.raw', function () {
-      var popupWindow = window.open('', 'RawCode', 'width=640, height=480');
-
-      popupWindow.document.write('<pre>' + _this.codeLines.join('\n') + '</pre>');
-      popupWindow.document.title = 'Raw code view';
-    });
+    this.$listing.on('click', '.raw', this.showRawCode.bind(this));
 
     return this;
   };
 
   CodeListing.prototype.buildListing = function () {
-    var linesNumberingWidth = this.numberingCharactersCount * this._options.monospaceCharacterWidth +
-      2 * this._options.horizontalPadding;
+    var numberingWidth = this.lines.length.toString().length * this.options.monospaceCharacterWidth +
+      2 * this.options.numberingHorizontalPadding;
 
     this.$listing
       .empty()
@@ -95,25 +72,25 @@
             })
           ),
 
-          this._options.lineNumbers ? $('<div>', {
+          this.options.numbering ? $('<div>', {
             'class': 'numbering',
-            style: 'width: ' + linesNumberingWidth + 'px;',
-            html: this.codeLines.map(function (code, i) {
-              return $('<span>', {
-                'data-line': this._options.numberingStart + i,
-                text: this._options.numberingStart + i
-              });
+            style: 'width: ' + numberingWidth + 'px;',
+            html: this.lines.map(function (line, i) {
+              return $('<span>', CodeListing.makeProps({
+                lineAttr: this.options.numberingStart + i,
+                text: this.options.numberingStart + i
+              }));
             }, this)
           }) : '',
 
           $('<div>', {
             'class': 'code',
-            style: 'margin-left: ' + (this._options.lineNumbers ? linesNumberingWidth : 0) + 'px;',
-            html: this.codeLines.map(function (code, i) {
-              return $('<span>', {
-                'data-line': this._options.numberingStart + i,
-                html: code
-              });
+            style: 'margin-left: ' + (this.options.numbering ? numberingWidth : 0) + 'px;',
+            html: this.lines.map(function (line, i) {
+              return $('<span>', CodeListing.makeProps({
+                lineAttr: this.options.numberingStart + i,
+                html: line
+              }));
             }, this)
           })
         )
@@ -122,14 +99,24 @@
     return this;
   };
 
-  CodeListing.prototype.clearActiveLine = function () {
-    this.$listing.find('span.active:not([data-line="' + this._persistentLine + '"])').removeClass('active');
+  CodeListing.prototype.setActiveLine = function (lineNumber) {
+    this.$listing
+      .find('[' + CodeListing.lineAttr + '="' + lineNumber + '"]')
+      .addClass('active');
   };
 
-  CodeListing.prototype.setActiveLine = function (lineNumber) {
-    if (!this._options.interactive) return;
+  CodeListing.prototype.clearActiveLine = function () {
+    var _this = this;
 
-    this.$listing.find('span[data-line="' + lineNumber + '"]').addClass('active');
+    this.$listing
+      .find('.active')
+      .each(function () {
+      var $line = $(this);
+
+      if (_this.persistentLines.indexOf(CodeListing.getLineNumber($line)) === -1) {
+        $line.removeClass('active');
+      }
+    });
   };
 
   CodeListing.prototype.toggleActiveLine = function (lineNumber) {
@@ -138,7 +125,51 @@
   };
 
   CodeListing.prototype.togglePersistentLine = function (lineNumber) {
-    this._persistentLine = this._persistentLine === lineNumber ? undefined : lineNumber;
+    var persistentLineIndex = this.persistentLines.indexOf(lineNumber);
+
+    if (persistentLineIndex > -1) {
+      delete this.persistentLines[persistentLineIndex];
+    } else {
+      this.persistentLines.push(lineNumber);
+    }
+
     this.setActiveLine(lineNumber);
+  };
+
+  CodeListing.prototype.showRawCode = function () {
+    var popupWindow = window.open('', 'RawCode', 'width=640, height=480');
+
+    popupWindow.document.write('<pre>' + this.lines.join('\n') + '</pre>');
+    popupWindow.document.title = 'Raw code view';
+  };
+
+
+  /**
+   * Static fields and methods.
+   */
+
+  CodeListing.defaultOptions = {
+    className: 'code-listing',
+    numbering: true,
+    numberingStart: 1,
+    interactive: true,
+    monospaceCharacterWidth: 10,
+    numberingHorizontalPadding: 10
+  };
+
+  CodeListing.lineAttr = 'data-code-listing-line';
+
+  CodeListing.getLineNumber = function (element) {
+    return parseInt($(element).attr(CodeListing.lineAttr), 10);
+  };
+
+  CodeListing.makeProps = function (props) {
+    var result = {};
+
+    Object.keys(props).map(function (propName) {
+      result[CodeListing.hasOwnProperty(propName) ? CodeListing[propName] : propName] = props[propName];
+    });
+
+    return result;
   };
 }(window));
